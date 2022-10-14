aa8e884fcb4a8070bea7dad594b80a21089c13ac
umijyi <umijyi@gmail.com>
2022-10-14 18:18:32 +0900

    oitokimasu

---
+++ ABC272_A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ABC272_A {
+    public static void main(String[] args) {
+        
+        System.out.println("入力してください。");
+
+        Scanner sc1 = new Scanner(System.in);
+        int a = sc1.nextInt();
+        int sum = 0;
+        for(int i = 0; i < a; i++){
+            sum += sc1.nextInt();
+        } 
+        System.out.println(a);
+        System.out.println(sum);
+    }
+}
